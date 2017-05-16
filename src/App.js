34cad52eb83d17c947b73fc467b2e319c876320d
Jesse Vollick <jessevollick@gmail.com>
2017-05-16 14:14:26 -0500

    app structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
 
+function ProcessTitle (props){
+  return <h1>{props.title}</h1>
+}
+
+var Timer = React.createClass({
+  render:function(){
+    return(
+      <h2>Timer goes In here</h2>
+    )
+  }
+});
+
+var Step = React.createClass({
+  render:function(){
+    var className = 'step';
+    return(
+      <div className={className}>
+        {this.props.index + 1}
+        {this.props.description}
+        {this.props.time}
+      </div>
+    )
+  }
+});
+
+var Steps = React.createClass({
+  render:function(){
+  var stepsList = [];
+  for(var i =0; i< this.props.numSteps;i ++){
+    stepsList.push(
+      <Step />
+    )
+  }
+
+    return(
+      <div className='steps'>{stepsList}</div>
+    )
+  }
+});
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Hello Markie</h2>
+        <div>
+          <ProcessTitle title='Clever Dripper' />
+          <Timer />
+          <Steps numSteps={6}></Steps>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
